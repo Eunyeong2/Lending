5d6e773d4ff1247823afebbac78cede0b4e0fc7a
Eunyeong2 <eunyeong.ahn@dream-academy.io>
2022-09-23 21:45:00 +0900

    lending

--- src/Lending.sol
+++ src/Lending.sol
@@ -9,14 +9,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyLend is IERC20, ERC20 {
 
-    DreamOracle oracle;
+    address private oracle;
     mapping(address => mapping(address => uint256)) public deposits; // 예금한 주소, 예금한 토큰, 예금한 값 저장
     mapping(address => mapping(address => uint256)) public borrows; // 빌린 주소, 빌린 토큰, 빌린 양 저장
+    mapping(address => mapping(address => uint256)) public interests; // tokenAddress , 빌린 계좌 => 이자 저장
     mapping(address => uint256) public times; // block.timestamp
+    mapping(address => uint256) public mortgages; //담보
     address public USDC;
+    address public ETH;
 
-    uint256 private reserve0;
-    uint256 private reserve1;
+    uint256 private total;
+    uint256 private total_deposits;
+    uint256 private total_interests;
 
     uint private unlocked = 1;
 
@@ -29,54 +33,101 @@ contract MyLend is IERC20, ERC20 {
         unlocked = 1;
     }
 
-    constructor(address _token0, address _oracle) ERC20("USD", "USDC"){
+    constructor(address _token0, address _token1, address _oracle) ERC20("USD", "USDC"){
         USDC = _token0;
-        oracle = DreamOracle(_oracle);
+        ETH = _token1;
         time = block.timestamp;
+        oracle = _oracle;
     }
 
-    function deposit(address tokenAddress, uint256 amount) external payable { // 입금
-        require(IERC20(tokenAddress).balanceOf(msg.sender) > amount, "msg.sender doesn't have enough amount!");
-        //deposits[msg.sender] = tokenAddress;
-        deposits[msg.sender][tokenAddress] = amount; //예금 정보 저장 완료
+    function give(address _who, address _token) public returns(uint256 __total){
+        __total = deposits[_who][_token];
+    }
+
+    function give2(address _who, address _token) public returns(uint256 ___total){
+        ___total = borrows[_who][_token];
+    }
+
+    function deposit(address tokenAddress, uint256 amount) external payable lock { // 입금
+        require(tokenAddress == USDC || tokenAddress == ETH, "tokenAddress is different");
+        //require(msg.value != 0, "msg.value is zero");
+        require(IERC20(tokenAddress).balanceOf(msg.sender) >= amount, "msg.sender doesn't have enough amount!");
+
+        if (tokenAddress == USDC){
+            deposits[msg.sender][tokenAddress] += amount; //예금 정보 저장 완료
+            total_deposits += amount;
+        } else{
+            mortgages[msg.sender] += amount; //담보 저장
+        }
         IERC20(tokenAddress).transferFrom(msg.sender, address(this), amount); // pool에 돈 전달
-        times[tokenAddress] = block.timestamp; // 입금한 시간 저장
     }
 
     function borrow(address tokenAddress, uint256 amount) external payable { //대출. tokenAddress : 담보 토큰. amount : 담보의 양
-        require(IERC20(tokenAddress).balanceOf(address(this)) > amount, "Token is under borrow amount"); //pool에 남아 있는 돈 계산
-        uint256 mortgage = oracle.getPrice(tokenAddress); //담보 가격 가져오기
-        //require(mortgage >= IERC20(USDC).balanceOf(address.this)); // 담보의 가격이 더 세야 함
+        require(IERC20(tokenAddress).balanceOf(address(this)) >= amount, "Token is under borrow amount"); //pool에 남아 있는 돈 계산
+        require(tokenAddress == USDC, "You can borrow only USDC");
+        
+        uint256 mortgage = DreamOracle(oracle).getPrice(ETH) * mortgages[msg.sender]; //담보 가격 가져오기
         uint256 limit = mortgage/2; //50% 비율로 빌려주기
+
         if (limit > amount){
             borrows[msg.sender][tokenAddress] = amount;
         } else{
             borrows[msg.sender][tokenAddress] = limit;
         }
+
         IERC20(USDC).transfer(msg.sender, borrows[msg.sender][tokenAddress]);
+
+        total+=borrows[msg.sender][tokenAddress];
+        times[msg.sender] = block.timestamp; // 빌린 시간 저장
     }
 
     function repay(address tokenAddress, uint256 amount) external payable { //상환
-        require(IERC20(tokenAddress).balanceOf(msg.sender) > amount, "msg.sender doesn't have enough amount!");
-        uint day = block.timestamp - times[tokenAddress] / 24; //이자율 계산을 위한 interests
-        borrows[msg.sender][tokenAddress] += (borrows[msg.sender][tokenAddress] * 1001 ^ day) / (1000 ^ day); //이자율 계산해서 총액 더함 (이자까지 합해서 갚아야 하니까)
+        require(IERC20(tokenAddress).balanceOf(msg.sender) >= amount, "msg.sender doesn't have enough amount!");
+        calculate(tokenAddress, times[msg.sender], block.timestamp); // 이자 갱신
+        give2(msg.sender, tokenAddress);
         IERC20(tokenAddress).transfer(address(this), amount); // 수수료 합쳐서 청산
-        require(amount < borrows[msg.sender][tokenAddress]);
-        IERC20(USDC).transfer(msg.sender, amount);
         borrows[msg.sender][tokenAddress] -= amount;
-        times[tokenAddress]=block.timestamp;
+        IERC20(ETH).transfer(msg.sender, amount);
+        times[msg.sender]=block.timestamp;
     }
 
     function liquidate(address user, address tokenAddress, uint256 amount) external payable { //청산
-        uint256 mortgage = oracle.getPrice(tokenAddress); //현재 담보 가격 가져오기
+        uint256 mortgage = DreamOracle(oracle).getPrice(tokenAddress); //현재 담보 가격 가져오기
         require(mortgage <= borrows[user][tokenAddress] * 3 / 4, "Liquidation threshold is 75%");
         _burn(msg.sender, borrows[msg.sender][tokenAddress]);
         borrows[user][tokenAddress] -= amount;
     }
 
     function withdraw(address tokenAddress, uint256 amount) external payable{
-        require(deposits[msg.sender][tokenAddress] >= amount, "Lack of deposit amounts");
-        deposits[msg.sender][tokenAddress] -= amount;
-        IERC20(tokenAddress).transfer(msg.sender, amount);
+        //require(deposits[msg.sender][tokenAddress] >= amount);
+        if( total_interests == 0){ // 축적된 이자가 없을 때 원금만 돌려줌.
+            require(deposits[msg.sender][tokenAddress] >= amount, "Lack of deposit amounts");
+            deposits[msg.sender][tokenAddress] -= amount;
+            IERC20(tokenAddress).transfer(msg.sender, amount);
+        } else {
+            require(deposits[msg.sender][tokenAddress] >= amount, "Lack of deposit amounts2");
+            if(deposits[msg.sender][tokenAddress] > amount){
+                calculate(tokenAddress,times[msg.sender], block.timestamp);
+                require((deposits[msg.sender][tokenAddress] + total_interests * (deposits[msg.sender][tokenAddress] / total_deposits)) >= amount, "Lack of deposit amounts");
+                deposits[msg.sender][tokenAddress] += total_interests * (deposits[msg.sender][tokenAddress] / total_deposits);
+                deposits[msg.sender][tokenAddress] -= amount + total_interests * (deposits[msg.sender][tokenAddress] / total_deposits);
+                IERC20(tokenAddress).transfer(msg.sender, amount + total_interests * (deposits[msg.sender][tokenAddress] / total_deposits));
+            } else{
+                calculate(tokenAddress,times[msg.sender], block.timestamp);
+                require((deposits[msg.sender][tokenAddress] + total_interests * (deposits[msg.sender][tokenAddress] / total_deposits)) >= amount, "Lack of deposit amounts");
+                deposits[msg.sender][tokenAddress] += total_interests * (deposits[msg.sender][tokenAddress] / total_deposits);
+                deposits[msg.sender][tokenAddress] -= amount + total_interests * (deposits[msg.sender][tokenAddress] / total_deposits);
+                IERC20(tokenAddress).transfer(msg.sender, amount + total_interests * (deposits[msg.sender][tokenAddress] / total_deposits));
+            }
+        }
+    }
+
+    function calculate(address tokenAddress, uint beforetime, uint aftertime) internal lock {
+        uint _days =  (aftertime - beforetime) / 1 days;
+        //uint _original = borrows[msg.sender][tokenAddress];
+        borrows[msg.sender][tokenAddress] = (borrows[msg.sender][tokenAddress] * (1001 ** _days)) / (1000 ** _days);
+        interests[msg.sender][tokenAddress] = (borrows[msg.sender][tokenAddress] * (1001 ** _days)) / (1000 ** _days);
+        //IERC20(tokenAddress).transfer(msg.sender, amount);
+        total_interests = interests[msg.sender][tokenAddress] - total;
     }
 }
--- test/lendtest.t.sol
+++ test/lendtest.t.sol
@@ -1,16 +1,274 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "../lib/forge-std/src/Test.sol";
-import "../src/Lending.sol";
+import "forge-std/Test.sol";
+import "src/Lending.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract coin is IERC20, ERC20{
-    constructor () ERC20("USD", "USDC"){   
+contract USDC is ERC20 {
+    uint public constant USDC_INITIAL_SUPPLY = 1000 ether;
+    constructor() ERC20("USD Coin", "USDC") {
+        super._mint(msg.sender, USDC_INITIAL_SUPPLY);
     }
 }
-contract MyLendTest is Test{
-    MyLend public mylend;
 
+contract ETH is ERC20 {
+    uint public constant USDC_INITIAL_SUPPLY = 1000 ether;
+    constructor() ERC20("ETH coin", "ETH") {
+        super._mint(msg.sender, USDC_INITIAL_SUPPLY);
+    }
+}
+
+contract LendingTest is Test {
+
+    MyLend bank;
+    DreamOracle oracle;
+    ERC20 usdc;
+    ERC20 eth;
+
+    function setUp() public {
+        oracle = new DreamOracle();
+        usdc = new USDC();
+        eth = new ETH();
+        bank = new MyLend(address(usdc), address(eth),address(oracle));
+        // set up initial USDC pool for bank
+        vm.deal(address(bank), 10 ether);
+        usdc.transfer(address(bank), 10 ether);   
+        oracle.setPrice(address(eth),10 ether);     
+
+    }
+
+    function testDepositBasic1() public {
+        address actor = address(0x11);
+        address borrower = address(0x12);
+        usdc.transfer(actor, 1 ether);
+        usdc.transfer(borrower, 1 ether);
+        uint256 balPrev;
+        uint256 balAfter;
+
+        vm.startPrank(actor);
+        vm.warp(0);
+        usdc.approve(address(bank), 1 ether);
+        bank.deposit(address(usdc), 1 ether);
+        vm.warp(1 days);
+        balPrev = usdc.balanceOf(actor);
+        bank.withdraw(address(usdc), 1 ether);
+        balAfter = usdc.balanceOf(actor);
+        assertEq(balAfter - balPrev, 1 ether);
+    }
+
+
+    function testDepositBasic2() public {
+        address actor = address(0x11);
+        vm.deal(actor, 20 ether);
+        uint256 balPrev;
+        uint256 balAfter;
+
+        vm.startPrank(actor);
+        vm.warp(0);
+        bank.deposit{value: 10 ether}(address(0), 0);
+        vm.warp(1 days);
+        balPrev = actor.balance;
+        bank.withdraw(address(0), 10.01 ether);
+        balAfter = actor.balance;
+        assertEq(balAfter - balPrev, 10.01 ether);
+    }
+
+    function testDepositBasic3() public {
+        address actor = address(0x11);
+        address borrower = address(0x12);
+        usdc.transfer(actor, 1 ether);
+        eth.transfer(borrower, 1 ether);
+        uint256 balPrev;
+        uint256 balAfter;
+        usdc.transfer(borrower,0.005 ether);
+
+        vm.startPrank(actor);
+        vm.warp(0);
+        usdc.approve(address(bank), 1 ether);
+        bank.deposit(address(usdc), 1 ether);
+        uint256 bal1 = usdc.balanceOf(address(bank)); // 11ether
+        bank.give(actor, address(usdc)); // 1ether
+        vm.stopPrank();
+
+        vm.startPrank(borrower);
+        vm.warp(0);
+        eth.approve(address(bank), 1 ether);
+        bank.deposit(address(eth), 1 ether);
+        bank.borrow(address(usdc), 0.5 ether);
+        bank.give2(borrower, address(usdc));
+        
+        vm.warp(1 days);
+
+        bank.give2(borrower, address(usdc));
+        usdc.balanceOf(borrower);
+        bank.repay(address(usdc), 0.5005 ether);
+        balPrev = usdc.balanceOf(actor);
+        bank.give(actor, address(usdc));
+        bank.withdraw(address(usdc), 0.5005 ether);
+        balAfter = usdc.balanceOf(actor);
+        assertEq(balAfter - balPrev, 0.5005 ether);
+    }
+
+    function testLendBasic() public {
+        address actor = address(0x11);
+        usdc.transfer(actor, 10 ether);
+        vm.deal(actor, 10 ether);
+        uint256 balPrev;
+        uint256 balAfter;
+        uint256 etherAmount;
+        oracle.setPrice(address(usdc), 2);
+
+        vm.startPrank(actor);
+        vm.warp(0);
+        balPrev = usdc.balanceOf(actor);
+        etherAmount = oracle.getPrice(address(usdc)) * 1 ether * 2;
+        bank.deposit{value: etherAmount}(address(0), 0);
+        bank.borrow(address(usdc), 1 ether);
+        balAfter = usdc.balanceOf(actor);
+        assertEq(balPrev + 1 ether, balAfter);
+        vm.warp(1 days);
+        
+        // first, repay 1 ether
+        balPrev = actor.balance;
+        usdc.approve(address(bank), 1.001 ether);
+        bank.repay(address(usdc), 1.000 ether);
+        balAfter = actor.balance;
+        assertEq(balPrev, balAfter);
+
+        // repay the interest, resulting in the collateral being returned
+        balPrev = actor.balance;
+        bank.repay(address(usdc), 0.001 ether);
+        balAfter = actor.balance;
+        assertEq(balPrev + etherAmount, balAfter);
+    }
+
+    function testLiquidateBasic1() public {
+        address actor1 = address(0x11);
+        address actor2 = address(0x22);
+        usdc.transfer(actor1, 10 ether);
+        usdc.transfer(actor2, 10 ether);
+        vm.deal(actor1, 10 ether);
+        uint256 balPrev;
+        uint256 balAfter;
+        uint256 etherAmount;
+        oracle.setPrice(address(usdc), 2);
+
+        vm.startPrank(actor1);
+        vm.warp(0);
+        balPrev = usdc.balanceOf(actor1);
+        etherAmount = oracle.getPrice(address(usdc)) * 1 ether * 2;
+        bank.deposit{value: etherAmount}(address(0), 0);
+        bank.borrow(address(usdc), 1 ether);
+        balAfter = usdc.balanceOf(actor1);
+        assertEq(balPrev + 1 ether, balAfter);
+        vm.warp(1 days);
+        vm.stopPrank();
+
+        // lower the price of ETH so that liquidation is triggered
+        oracle.setPrice(address(usdc), 4);
+        vm.startPrank(actor2);
+        balPrev = usdc.balanceOf(actor2);
+        usdc.approve(address(bank), 10 ether);
+        bank.liquidate(actor1, address(usdc), etherAmount);
+        balAfter = usdc.balanceOf(actor2);
+        assertGt(balPrev, balAfter);
+        vm.stopPrank();
+    }
+
+    function testLiquidateBasic2() public {
+        address actor1 = address(0x11);
+        address actor2 = address(0x22);
+        usdc.transfer(actor1, 10 ether);
+        usdc.transfer(actor2, 10 ether);
+        vm.deal(actor1, 10 ether);
+        uint256 balPrev;
+        uint256 balAfter;
+        uint256 etherAmount;
+        oracle.setPrice(address(usdc), 2);
+
+        vm.startPrank(actor1);
+        vm.warp(0);
+        balPrev = usdc.balanceOf(actor1);
+        etherAmount = oracle.getPrice(address(usdc)) * 1 ether * 2;
+        bank.deposit{value: etherAmount}(address(0), 0);
+        bank.borrow(address(usdc), 1 ether);
+        balAfter = usdc.balanceOf(actor1);
+        assertEq(balPrev + 1 ether, balAfter);
+        vm.warp(1 days);
+        vm.stopPrank();
+
+        // liquidation is not triggered
+        vm.startPrank(actor2);
+        balPrev = usdc.balanceOf(actor2);
+        usdc.approve(address(bank), 10 ether);
+        bank.liquidate(actor1, address(usdc), etherAmount);
+        balAfter = usdc.balanceOf(actor2);
+        assertEq(balPrev, balAfter);
+        vm.stopPrank();
+    }
+
+    function testDoubleDeposit() public {
+        address actor = address(0x11);
+        uint256 balPrev;
+        uint256 balAfter;
+        usdc.transfer(actor, 10 ether);
+        oracle.setPrice(address(usdc), 2);
+
+        vm.startPrank(actor);
+        usdc.approve(address(bank), 2 ether);
+        balPrev = usdc.balanceOf(address(bank));
+        bank.deposit(address(usdc), 1 ether);
+        bank.deposit(address(usdc), 1 ether);
+        balAfter = usdc.balanceOf(address(bank));
+        assertEq(balAfter, balPrev + 2 ether);
+    }
+
+    function testLiquidatePartial() public {
+        address actor1 = address(0x11);
+        address actor2 = address(0x22);
+        address actor3 = address(0x33);
+        usdc.transfer(actor1, 10 ether);
+        usdc.transfer(actor2, 10 ether);
+        usdc.transfer(actor3, 10 ether);
+        vm.deal(actor1, 10 ether);
+        uint256 balPrev;
+        uint256 balAfter;
+        uint256 etherAmount;
+        uint256 liquidateUsdc1;
+        uint256 liquidateUsdc2;
+        oracle.setPrice(address(usdc), 2);
+
+        vm.startPrank(actor1);
+        vm.warp(0);
+        balPrev = usdc.balanceOf(actor1);
+        etherAmount = oracle.getPrice(address(usdc)) * 1 ether * 2;
+        bank.deposit{value: etherAmount}(address(0), 0);
+        bank.borrow(address(usdc), 1 ether);
+        balAfter = usdc.balanceOf(actor1);
+        assertEq(balPrev + 1 ether, balAfter);
+        vm.warp(1 days);
+        vm.stopPrank();
+
+        // lower the price of ETH so that liquidation is triggered
+        oracle.setPrice(address(usdc), 4);
+        vm.startPrank(actor2);
+        usdc.approve(address(bank), 10 ether);
+        balPrev = usdc.balanceOf(actor2);
+        bank.liquidate(actor1, address(usdc), etherAmount / 2);
+        balAfter = usdc.balanceOf(actor2);
+        liquidateUsdc1 = balPrev - balAfter;
+        vm.stopPrank();
+
+        vm.startPrank(actor3);
+        usdc.approve(address(bank), 10 ether);
+        balPrev = usdc.balanceOf(actor3);
+        bank.liquidate(actor1, address(usdc), etherAmount / 4);
+        balAfter = usdc.balanceOf(actor3);
+        liquidateUsdc2 = balPrev - balAfter;
+        vm.stopPrank();
+        
+        assertEq(liquidateUsdc1, liquidateUsdc2 * 2);
+    }
 }
