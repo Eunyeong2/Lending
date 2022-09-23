8905e2e4f89e8c309f9074c89df09f2df0e1096b
Eunyeong2 <eunyeong.ahn@dream-academy.io>
2022-09-23 13:20:07 +0900

    edit..

--- src/lending.sol
+++ src/lending.sol
@@ -10,8 +10,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MyLend is IERC20, ERC20 {
 
     DreamOracle public oracle;
-    mapping(address => uint256) public interests; // 이자 계산한 값 금액 저장
-    mapping(address => uint256) public borrows; // 빌린 금액 저장
+    mapping(address => mapping(address => uint256)) public deposits; // 예금한 주소, 예금한 토큰, 예금한 값 저장
+    mapping(address => mapping(uint256 => uint256)) public borrows; // 빌린 주소, 빌린 값, 이자 값 저장
     mapping(address => uint256) public times; // block.timestamp
     //uint time; // 현재 시간
     address public ETH;
@@ -37,38 +37,46 @@ contract MyLend is IERC20, ERC20 {
         time = block.timestamp;
     }
 
-    function deposit(address tokenAddress, uint256 amount) external { //입금
+    function deposit(address tokenAddress, uint256 amount) external { // 입금
         require(IERC20(tokenAddress).balanceOf(msg.sender) > amount, "msg.sender doesn't have enough amount!");
-        IERC20(tokenAddress).transfer(address(this), amount);
-        times[tokenAddress] = block.timestamp;
+        deposits[msg.sender] = tokenAddress;
+        deposits[msg.sender][tokenAddress] = amount; //예금 정보 저장 완료
+        IERC20(tokenAddress).transfer(address(this), amount); // pool에 돈 전달
+        times[tokenAddress] = block.timestamp; // 입금한 시간 저장
     }
 
-    function calculate(address tokenAddress) internal returns(uint256 _reserve) {
-        uint days = (block.timestamp - times[tokenAddress]) / 24 ;
-        for (i=0;i<days;i++){
-            _reserve = IERC20(tokenAddress).balanceOf(address(this));
-            _reserve = (_reserve * 1001 ^ days) / 1000 ^ days;
-        }
-        interests[tokenAddress] = _reserve;
-        borrows[msg.sender] += _reserve;
-    }
+    // function calculate(address tokenAddress) internal returns(uint256 _reserve) {
+    //     uint days = (block.timestamp - times[tokenAddress]) / 24;
+    //     for (i=0;i<days;i++){
+    //         _reserve = IERC20(tokenAddress).balanceOf(address(this));
+    //         _reserve = (_reserve * 1001 ^ days) / 1000 ^ days;
+    //     }
+    //     interests[tokenAddress] = _reserve;
+    //     borrows[msg.sender] += _reserve;
+    // }
 
-    function borrow(address tokenAddress, uint256 amount) external { //대출
-        //tokenAddress : 담보. amount : 대출할 양
+    function borrow(address tokenAddress, uint256 amount) external { //대출. tokenAddress : 담보 토큰. amount : 담보의 양
+        require(IERC20(tokenAddress).balaceOf(address(this)) > amount, "Token is under borrow amount"); //pool에 남아 있는 돈 계산
         uint256 mortgage = oracle.getPrice(tokenAddress); //담보 가격 가져오기
         require(mortgage >= IERC20(USDC).balanceOf(address.this)); // 담보의 가격이 더 세야 함
-        IERC20(USDC).transfer(msg.sender, mortgage/2); //50% 비율로 빌려주기
-        borrows[msg.sender] += mortgage/2;
+        uint256 limit = mortgage/2; //50% 비율로 빌려주기
+        if (limit > amount){
+            borrows[msg.sender][tokenAddress] = amount;
+        } else{
+            borrows[msg.sender][tokenAddress] = limit;
+        }
+        IERC20(USDC).transfer(msg.sender, borrows[msg.sender][tokenAddress]);
     }
 
     function repay(address tokenAddress, uint256 amount) external { //상환
         require(IERC20(tokenAddress).balanceOf(msg.sender) > amount, "msg.sender doesn't have enough amount!");
-        uint days = block.timestamp - time / 24; //이자율 계산을 위한 interests
+        uint days = block.timestamp - time2 / 24; //이자율 계산을 위한 interests
+        borrows[msg.sender][tokenAddress] += (borrows[msg.sender][tokenAddress] * 1001 ^ days) / 1000 ^ days; //이자율 계산해서 총액 더함
         IERC20(tokenAddress).transfer(address(this), amount); // 수수료 합쳐서 청산
-        borrows[msg.sender] -= amount;
+        borrows[msg.sender][tokenAddress] -= amount;
+        uint time2 = block.timestamp;
     }
 
     function liquidate(address user, address tokenAddress, uint256 amount) external { //청산
-
     }
 }
